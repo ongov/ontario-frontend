333f453a1a703650f19607bdbe21182aae987ef6
kyle-binger-3 <164547352+kyle-binger-3@users.noreply.github.com>
2024-04-16 10:28:24 -0400

    ONFRONT-35 - Updated Design System to v1.7.0. (#23)
    - CLI
      - added new id of `ontario-header-menu-icon` on the `<use>` tag within the `<svg>` inside of `_header_menu_open_button.njk`
        - **Note:** this is a breaking change, without the menu button will not work
    - ONTARIO-FRONTEND
      - DS
        - `ds-theme.css` has been removed, now only keeping the minified version `ds-theme.min.css`
        - `package.json` file added which contains version information about the installed Design System version
    
    Co-authored-by: Matt St Jean <112188737+matt-stjean@users.noreply.github.com>

--- packages/ontario-frontend/src/assets/vendor/ontario-design-system/scripts/ontario-search.js
+++ packages/ontario-frontend/src/assets/vendor/ontario-design-system/scripts/ontario-search.js
@@ -3,3 +3,46 @@ function doSearch() {
 	var searchInputField = document.getElementById('ontario-search-input-field');
 	console.log('search input field:', searchInputField.value);
 }
+
+function handleSearch() {
+	console.log('Searching...');
+}
+
+/**
+ * Add a click listener to the search reset button to clear and focus the search input.
+ *
+ * @param {string} searchResetButtonId Id of the search reset button
+ * @param {string} searchInputId Id of the search input
+ */
+function addSearchResetOnClickListener(searchResetButtonId, searchInputId) {
+	document.getElementById(searchResetButtonId).addEventListener('click', () => {
+		const searchInput = document.getElementById(searchInputId);
+		if (searchInput) {
+			// Reset the search box contents
+			const searchResetButton = document.getElementById(searchResetButtonId);
+			searchInput.value = '';
+			searchResetButton.style.display = 'none';
+
+			// Focus the search box
+			searchInput.focus();
+		}
+	});
+}
+
+/**
+ * Add an input event listener to the search input to show/hide the search reset button.
+ *
+ * @param {string} searchInputId Id of the search input
+ * @param {string} searchResetButtonId Id of the search reset button
+ */
+function addResetButtonOnSearchInputListener(searchResetButtonId, searchInputId) {
+	const resetButton = document.getElementById(searchResetButtonId);
+	const searchInput = document.getElementById(searchInputId);
+	document.getElementById(searchInputId)?.addEventListener('input', function () {
+		if (searchInput.value !== '') {
+			resetButton.style.display = 'block';
+		} else {
+			resetButton.style.display = 'none';
+		}
+	});
+}
