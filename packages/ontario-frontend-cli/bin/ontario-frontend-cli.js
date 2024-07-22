9955e762e919d4baf93da45ef2a7d15943faddb2
Matt St Jean <matt.st.jean@ontario.ca>
2024-07-22 08:18:26 -0400

    fix(ONFRONT-91): use abs paths

--- packages/ontario-frontend-cli/bin/ontario-frontend-cli.js
+++ packages/ontario-frontend-cli/bin/ontario-frontend-cli.js
@@ -1,15 +1,16 @@
 #!/usr/bin/env node
 
 const { spawnSync } = require('child_process');
+const path = require('path');
 
 const args = process.argv.slice(2);
 
 const command = args[0];
 
 const scriptMap = {
-  'ontario-create-app': './ontario-create-app.js',
-  'ontario-add-package': './ontario-add-package.js',
-  'ontario-remove-package': './ontario-remove-package.js',
+  'ontario-create-app': path.join(__dirname, 'ontario-create-app.js'),
+  'ontario-add-package': path.join(__dirname, 'ontario-add-package.js'),
+  'ontario-remove-package': path.join(__dirname, 'ontario-remove-package.js'),
 };
 
 if (scriptMap[command]) {
